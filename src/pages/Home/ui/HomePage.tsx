e5c7b34fb3c575dbfd2fc5c44f39e1bbacc2aafb
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-01-18 14:58:21 +0300

    add i18n

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router";
 import styles from "./HomePage.module.scss";
 import { useTheme } from "@/shared/config";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
   const { toggleTheme } = useTheme();
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    i18n.changeLanguage(i18n.language === "en" ? "ru" : "en");
+  };
+
   return (
     <>
-      <h1 className={styles.title}>Home</h1>
+      <h1 className={styles.title}>{t("hello")}</h1>
       <button onClick={toggleTheme}>theme</button>
+      <button onClick={changeLanguage}>language</button>
       <Link to={"/login"}>Login page</Link>
     </>
   );
