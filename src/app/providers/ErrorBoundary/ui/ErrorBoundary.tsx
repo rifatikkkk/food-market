af3d68cdbc181dbfa63d57d1d2c5c03454d406f6
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-01-29 19:34:12 +0300

    add Not Found page

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -18,7 +18,7 @@ export class ErrorBoundary extends React.Component<
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error: Error) {
+  static getDerivedStateFromError() {
     return { hasError: true };
   }
 
--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,25 +1,12 @@
-import { Link } from "react-router";
 import styles from "./HomePage.module.scss";
-import { useTranslation } from "react-i18next";
 import { Header } from "@/widgets/Header";
 import { Footer } from "@/widgets/Footer";
-import { Button, Spinner } from "@/shared/ui";
-import { useEffect, useState } from "react";
 
 const HomePage = () => {
-  const { t } = useTranslation();
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    if (error) throw new Error();
-  }, [error]);
-
   return (
     <div className={styles.pageWrapper}>
       <Header />
-      <main className={styles.content}>
-        <Button onClick={() => setError(true)}>Click</Button>
-      </main>
+
       <Footer />
     </div>
   );
