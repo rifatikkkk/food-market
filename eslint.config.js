fde7a61a80b75e846c87c6c7d3dc07b03ee57679
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-01-30 07:11:36 +0300

    ci

--- eslint.config.js
+++ eslint.config.js
@@ -8,24 +8,28 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 
-export default defineConfig([globalIgnores(["dist"]), {
-  files: ["**/*.{ts,tsx}"],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argIgnorePattern: "^_",
-      },
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
     ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
-}, ...storybook.configs["flat/recommended"]]);
+  ...storybook.configs["flat/recommended"],
+]);
