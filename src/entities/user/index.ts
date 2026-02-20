2eb9cc62f0a261543fe67b6b9b9f2887cb1fe7dd
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-02-20 16:24:27 +0300

    apply user session

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,6 +1,7 @@
+import { applyUserSession } from "./model/services/applyUserSession/applyUserSession";
 import { refreshSession } from "./model/services/refreshSession/refreshSession";
 import { userActions, userReducer } from "./model/slice/userSlice";
 import type { User, UserSchema } from "./model/types/UserSchema";
 
-export { userActions, userReducer, refreshSession };
+export { userActions, userReducer, refreshSession, applyUserSession };
 export type { User, UserSchema };
--- src/features/login/model/services/login/login.ts
+++ src/features/login/model/services/login/login.ts
@@ -1,9 +1,8 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import { userActions } from "@/entities/user";
+import { applyUserSession } from "@/entities/user";
 
 import { extractErrorMessage, httpClient } from "@/shared/api";
-import { LOCAL_STORAGE_USER_KEY } from "@/shared/config";
 
 type LoginArgs = {
   email?: string;
@@ -18,8 +17,7 @@ export const login = createAsyncThunk<void, LoginArgs, { rejectValue: string }>(
       const res = await httpClient.post("/auth/login", authData);
       const user = res.data;
 
-      localStorage.setItem(LOCAL_STORAGE_USER_KEY, JSON.stringify(user));
-      thunkApi.dispatch(userActions.setUserData(user));
+      applyUserSession(user, thunkApi.dispatch);
       return;
     } catch (error) {
       return thunkApi.rejectWithValue(extractErrorMessage(error));
